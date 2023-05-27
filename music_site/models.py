2959c61e8d613c72ed02b5b9b8d07c8a791d95a1
Federallo <aligiassioma72@gmail.com>
2023-05-27 16:52:16 +0200

    created models for streaming music service

--- music_site/models.py
+++ music_site/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class Songs(models.Model):
+    title = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+    album = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    year = models.IntegerField()
+    length = models.IntegerField()
+    image = models.ImageField(upload_to='images/')
+    file = models.FileField(upload_to='songs/')
+
+    def __str__(self):
+        return self.title + ' - ' + self.artist
+    
+class Playlist(models.Model):
+    name = models.CharField(max_length=50)
+    songs = models.ManyToManyField(Songs)
+
+    def __str__(self):
+        return self.name
+    
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    playlists = models.ManyToManyField(Playlist)
+
+    def __str__(self):
+        return self.username
+    
+class Recommendations(models.Model):
+    song = models.ForeignKey(Songs, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    playlists = models.ManyToManyField(Playlist)
+
+    def __str__(self):
+        return self.song.title + ' - ' + self.user.username + ' - ' + self.rating
