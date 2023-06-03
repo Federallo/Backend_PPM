1c6c8993e3afd49e508cb4bd92c490684d1c0188
Andrea <aligiassioma72@gmail.com>
2023-06-03 19:23:15 +0200

    first homepage implementation

--- music_streaming/music_streaming/settings.py
+++ music_streaming/music_streaming/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local apps
     'music_site',
 ]
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'music_streaming.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
