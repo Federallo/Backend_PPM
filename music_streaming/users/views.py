c49992bdc4e497dc5d4f38de322cc0ce5598d187
Andrea <aligiassioma72@gmail.com>
2023-06-04 17:36:52 +0200

    created views for creating, deleting, editing a playlist. Also added html files for interfaces

--- music_streaming/users/views.py
+++ music_streaming/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import MusicUser
 
 # Create your views here.
+class UserInterfraceView(ListView):
+    model = MusicUser
+    template_name = 'userProfile.html'
+
+class UserCreatePlaylistView(ListView):
+    model = MusicUser
+    template_name = 'playlistCreation.html'
+
+class UserDeletePlaylistView(ListView):
+    model = MusicUser
+    template_name = 'playlistDelete.html'
+
+class UserEditPlaylistView(ListView):
+    model = MusicUser
+    template_name = 'playlistEdit.html'
