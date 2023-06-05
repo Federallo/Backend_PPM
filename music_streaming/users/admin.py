bb6b52e680b040e3a0fe7625e7b7494c60ff26df
Federallo <aligiassioma72@gmail.com>
2023-06-05 09:07:33 +0200

    added custom user in settings and in admin app

--- music_streaming/users/admin.py
+++ music_streaming/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import MusicUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+admin.site.register(MusicUser, UserAdmin)
