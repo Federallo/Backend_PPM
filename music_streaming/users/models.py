a1630b17d8f5f3d15368330b45854904ec602495
Federallo <aligiassioma72@gmail.com>
2023-06-05 09:39:10 +0200

    added user manager for MusicUser

--- music_streaming/users/models.py
+++ music_streaming/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 # Create your models here.
@@ -6,6 +6,20 @@ class MusicUser(AbstractUser):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
+
+class MsuicUserManager(BaseUserManager):
+    def create_user(self, name, surname, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            name = name,
+            surname = surname,
+            email = self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
 
 class Playlist(models.Model):
     name = models.CharField(max_length=50)
