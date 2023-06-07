59c81efddc8d4246e856c89b6a3e0ae889a175e9
Andrea <aligiassioma72@gmail.com>
2023-06-07 11:40:47 +0200

    removed junk stuff

--- music_streaming/users/urls.py
+++ music_streaming/users/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .views import SignUpView#, user_create_form, UserInterfaceView, user_login_form, PlaylistsView, UserCreatePlaylistView, UserDeletePlaylistView, DetailedPlaylistView
+from .views import SignUpView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    #path('login/', user_login_form, name='userLogin'),
-    #path('create/', user_create_form, name='userCreate'),
-    #path('profile/', UserInterfaceView.as_view(), name='userProfile'),
 ]
