f88c6483f807a535251d405ccb5265de6c542d63
Andrea <aligiassioma72@gmail.com>
2023-06-07 14:05:06 +0200

    added playlist model in songs

--- music_streaming/songs/models.py
+++ music_streaming/songs/models.py
@@ -8,9 +8,16 @@ class Songs(models.Model):
     genre = models.CharField(max_length=50)
     year = models.IntegerField()
     length = models.IntegerField()
-    #TODO maybe remove this
+    
     def __str__(self):
         return self.title + ' - ' + self.artist
+    
+class Playlist(models.Model):
+    name = models.CharField(max_length=50)
+    songs = models.ManyToManyField(Songs)
+    
+    def __str__(self):
+        return self.name
     
 class Recommendation(models.Model):
     name = models.CharField(max_length=50)
