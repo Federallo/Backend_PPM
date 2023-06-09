f4c65aa07a0f120f9beb20afe3933ebd7d5ea21e
Andrea <aligiassioma72@gmail.com>
2023-06-09 21:04:54 +0200

    updated PlaylistForm

--- music_streaming/songs/forms.py
+++ music_streaming/songs/forms.py
@@ -4,7 +4,7 @@ from .models import Playlist, Songs #TODO implement songs adding feature
 class PlaylistForm(forms.ModelForm):
     class Meta:
         model = Playlist
-        fields = ('name', 'description', 'songs')
+        fields = ('name', 'description',)
         widgets = {
             'owner': forms.HiddenInput(),
         }
