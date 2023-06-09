a9ef9f63d91f77bc2d26bed237f807eaa70add0f
Andrea <aligiassioma72@gmail.com>
2023-06-09 23:16:31 +0200

    created song serializer for songs filtering

--- music_streaming/songs/models.py
+++ music_streaming/songs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from rest_framework import serializers
 
 # Create your models here.
 class Songs(models.Model):
@@ -28,3 +29,8 @@ class Recommendation(models.Model):
 
     def __str__(self):
         return self.name
+    
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Songs
+        fields = ('title', 'artist', 'album', 'genre', 'year', 'length')
