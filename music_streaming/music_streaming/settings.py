eb72a61c4e554a59aa41eb8bc5c11c9c6ffdf6e3
Andrea <aligiassioma72@gmail.com>
2023-06-11 12:56:12 +0200

    updated project for railway deployment

--- music_streaming/music_streaming/settings.py
+++ music_streaming/music_streaming/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,7 @@ SECRET_KEY = 'django-insecure-*l(scap^=sriw=a7j4330a1)l87z@7ajyx5wq%g+*5s3rusy6%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
